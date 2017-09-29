a1459676ad00ff3de5ae7d5597ee704d927ed023
Steven <s.a.green51@gmail.com>
2017-09-28 19:48:29 -0700

    3rd

--- prog2_3.c
+++ prog2_3.c
@@ -1,8 +1,71 @@
-#include <stdin>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_STR_SIZE 265
 
 void main (){
 
+	char input[MAX_STR_SIZE];
+	char *temp;
+	char output[MAX_STR_SIZE];
 
+	printf("Assignment #2-3, Steven Green, s.a.green51@gmail.com\n");
+for(;;){
+    printf(">");
+	fgets(input,MAX_STR_SIZE,stdin);
 
+	input[(strlen(input))] =  '\0';
 
+//    temp = strtok(input, " ");
+
+    int wordCount = 0;
+
+	int length = strlen(input);
+
+    for (int i = 0; i < length; i++){
+        
+        int charCounter = 0;  
+        int j;    
+  
+        if (charCounter == 65)break;
+        if (input[i] == '\n' || input[i] == '\0') break;
+        if (input[i] != ' '){
+            charCounter++;            
+            if (isdigit(input[i])){
+                if (isdigit(input[i+1])||input[i+1] == ' '){
+                  //  printf("INT ");          
+                    j = i;                
+                    while(input[j] != ' ')j++;
+                    charCounter+=j-i;
+                    i = j;
+                    wordCount++;
+                    strcat(output,"INT ");
+                }
+            }
+                else{
+                    j = i;                
+                    while(input[j] != ' ')j++;
+                    charCounter+=j-i;
+                    i = j;
+                    wordCount++;
+                    strcat(output,"STR ");
+                  //  printf("STR ");
+                }
+        }
+    }
+        if (wordCount > 2) printf("ERROR! Incorrect number of tokens found\n");            
+        else  printf("%s\n",output);
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
