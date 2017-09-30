31d7e03cad8e6e31cdda1b1fb77f8c0af51069aa
Steven <s.a.green51@gmail.com>
2017-09-29 17:54:43 -0700

    test

--- prog2_3.c
+++ prog2_3.c
@@ -10,66 +10,60 @@ void main (){
 	char output[MAX_STR_SIZE];
 
 	printf("Assignment #2-3, Steven Green, s.a.green51@gmail.com\n");
+
 for(;;){
+
     printf(">");
-	fgets(input,MAX_STR_SIZE,stdin);
-
-	input[(strlen(input))] =  '\0';
-
-//    temp = strtok(input, " ");
-
-    int wordCount = 0;
-
+    fgets(input,MAX_STR_SIZE,stdin);
+    input[(strlen(input))-1] =  ' ';
 	int length = strlen(input);
+    int numWords = 0;
 
     for (int i = 0; i < length; i++){
-
+        
         int charCounter = 0;  
         int j;    
-      if (input[0] == ' ')wordCount = 3;
+  
         if (input[i] != ' '){
             charCounter++;            
             if (isdigit(input[i])){
                 if (isdigit(input[i+1])||input[i+1] == ' '){
                     j = i;                
                     while(input[j] != ' ')j++;
-                    charCounter+=j-i;
+                    charCounter+=(j-i);
+                    if (charCounter > 65){
+                        numWords = 3;
+                        break; 
+                        }
                     i = j+1;
-                    wordCount++;
-                    strcat(output,"INT ");
+                    numWords++;
+                    strcat(output, "INT ");
                 }
                 else{
+                    numWords++;
                     strcat(output,"INT ");
-                    break;
-                    }            
-            }
-        else{
-               j = i;                
-               while(input[j] != ' ')j++;
-               charCounter+=j-i;
-               i = j+1;
-               wordCount++;
-               if (input[j+1] != '\0' ) strcat(output,"STR ");
-               }
-        }
-         if (charCounter == 65)break;
-         if (input[i] == '\n' || input[i] == '\0') break;
-    }
-        if (wordCount > 2) printf("ERROR! Incorrect number of tokens found\n");            
+                }            
+           }
+           else { 
+                    j = i;                
+                    while(input[j] != ' ')j++;
+                    charCounter+=(j-i);
+                    if (charCounter > 65){
+                        numWords = 3;
+                        break; 
+                        }
+                    i = j+1;
+                    strcat(output,"STR ");
+                    numWords++;                 
+           }
+       }            
+ 
+        if (charCounter == 65)break;
+        if (input[i] == '\n' || input[i] == '\0') break;
+   }
+        if (numWords > 2) printf("ERROR! Incorrect number of tokens found\n");            
         else  printf("%s\n",output);
         output[0] = '\0';
         output[1] = '\0';
 }
 }
-
-
-
-
-
-
-
-
-
-
-
-
