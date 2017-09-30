98f0f16f826e9027bb418e78eaf4507994e19cbe
Steven <s.a.green51@gmail.com>
2017-09-29 21:01:08 -0700

    test

--- prog2_5.c
+++ prog2_5.c
@@ -1,8 +1,88 @@
-#include <stdin>
+#include <stdio.h>
+#include <string.h>
+#include <math.h> 
 
-void main(){
+#define MAX_STR_SIZE 265
 
+void main (){
 
+	char input[MAX_STR_SIZE];
+	char output[MAX_STR_SIZE];
+    char *temp;
+    int length, numWords, charCounter, numChar, j;
+    int counter = 0;    
 
+	printf("Assignment #2-5, Steven Green, s.a.green51@gmail.com\n");
 
+for(;;){
+   // if (counter == 4) break;
+    printf("> ");
+    fgets(input,MAX_STR_SIZE,stdin);
+    input[(strlen(input))-1] =  '\0';
+	length = strlen(input);
+    numWords = 0;
+    charCounter = 0; 
+    numChar = 0;
+    
+    if(input[0] == 'q' ||input[0] == 'Q' )if(input[1] == 'U'||input[1] == 'u')if( input[2] == 'i'||input[2] == 'I')if(input[3] == 't'||input[3] == 'T' ) break;
+        
+    for (int p = 0; p <length; p++) numChar++; 
+
+    for (int i = 0; i < length; i++){  
+           
+        
+        if(input[0] == ' ' && input[1] == '\0'){
+            numWords =3;
+            break;
+            } 
+
+        if (input[i] != ' '){
+            if (isdigit(input[i])){
+                if (isdigit(input[i+1])||input[i+1] == ' '){
+                    numWords++;
+                    strcat(output, "INT ");          
+                    j = i;                
+                    while(input[j] != ' '){
+                        j++;
+                        charCounter++;
+                        }                                      
+                    if (j==length-1) break;
+                    i = j;
+                }
+                else{
+                    numWords++;
+                    strcat(output,"INT ");
+                    break;
+                }            
+           }
+           else { 
+                    j = i;                
+                    while(input[j] != ' '){
+                        j++;
+                        charCounter++;
+                        }
+                        i = j;
+                    if (input[j] != '\0') strcat(output,"STR ");
+                    numWords++;                 
+           }
+       }        
+        if (input[i] == '\n' || input[i] == '\0') {     
+            numWords = 3;  
+            break;
+            }
+   }
+        if (numChar > 65 )printf("ERROR! Input string too long.\n");
+        else if (numWords > 2||numWords == 0) printf("ERROR! Incorrect number of tokens found.\n");   
+        else if (output[0] == 'I'&& output[4] != '\0' ) {
+            strcpy(output,"ERROR: Expected STR INT.");
+            printf("%s\n",output);
+            }  
+        else if (output[0] == 'I') {
+            strcpy(output,"ERROR: Expected STR.");
+            printf("%s\n",output);
+            }        
+        else  printf("%s\n",output);
+        for (int y = 0; y < 7; y++) output[y] = '\0';
+   //     counter++;
+}
 }
